7bd927776b2d237b0a85131ec7075d9d14585933
Peter Rose <peterroseq1@gmail.com>
2020-03-14 19:14:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,47 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(array)
+  counter = 0 
+  while counter < array.length    
+    array.each_with_index do |name, i| puts "#{i+1}. " + "#{name}"
+    counter += 1 
+   end 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def summon_captain_planet(planeteer_calls)
+ planeteer_calls.map! do |element| element.capitalize + "!"
+  end 
+end 
+
+
+def long_planeteer_calls(array)
+  array.any? do |call| call.length > 4
+  end
+end 
+
+
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  cheese_types.each do |the_cheese|
+ 
+  if foods.include?(the_cheese)
+    return the_cheese
+ end 
+ end
+   nil
+ 
+end 
+foods = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(foods)
+
+
+array = ["Dopey", "Grumpy", "Bashful"]
+roll_call_dwarves(array)
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+summon_captain_planet(planeteer_calls)
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
+foods = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(foods)
